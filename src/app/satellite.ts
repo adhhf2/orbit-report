f594578d545ce9dbcf9cd0bd4af651f8474bad43
adhhf2 <adhhf2@gmail.com>
2020-09-24 00:14:29 -0500

    initial commit

---
+++ src/app/satellite.ts
@@ -0,0 +1,35 @@
+import { FunctionCall } from '@angular/compiler';
+
+export class Satellite {
+name: string;
+orbitType: string;
+type: string;
+operational: boolean;
+launchDate: string;
+shouldShowWarning: Function;
+constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean){
+    this.name = name;
+    this.orbitType = orbitType;
+    this.type = type;
+    this.operational = operational;
+    this.launchDate = launchDate;
+    this.shouldShowWarning = function():boolean{
+        if(this.type.toLowerCase()==='space debris'){
+            return true
+        }
+        else{
+            return false
+        }
+    }
+    console.log(this.shouldShowWarning(this.type));
+    }
+//     shouldShowWarning():boolean{
+//     if(this.type.toLowerCase()==='space debris'){
+//         return true
+//         }
+//         else{
+//             return false
+//         }
+// }
+
+}
